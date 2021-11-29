af0b68e80d9d4ad8586ff0ee0d7c2d8a73015b83
Bryan Anderson <banderson5144@gmail.com>
2021-11-29 09:54:23 -0500

    Try and download csv

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,34 +1,47 @@
 import { LightningElement, track } from 'lwc';
 import _ from 'lodash';
+import parse from 'json2csv';
 
-export default class App extends LightningElement
-{
+export default class App extends LightningElement {
     success = false;
     isSet = false;
     columns = [
         { label: 'sObject API Name', fieldName: 'name' },
-        { label: 'Count', fieldName: 'count' },
+        { label: 'Count', fieldName: 'count' }
     ];
-    @track tblData = [{name:'foo',count:'1'}];
+    @track tblData = [{ name: 'foo', count: '1' }];
 
-    connectedCallback()
-    {
+    connectedCallback() {
         var searchParams = new URLSearchParams(window.location.search);
 
-        if(searchParams != null && searchParams.has('success'))
-        {
+        if (searchParams != null && searchParams.has('success')) {
             this.isSet = true;
             this.success = searchParams.get('success') === 'true';
         }
     }
 
-    handleClick(evt)
-    {
+    handleClick(evt) {
         fetch('/getcounts')
-        .then(response => response.json())
-        .then(data =>{
-            this.tblData = _.orderBy(data.sObjects,'count', 'desc');
-            console.log(this.tblData);            
-        });
+            .then((response) => response.json())
+            .then((data) => {
+                this.tblData = _.orderBy(data.sObjects, 'count', 'desc');
+                //console.log(this.tblData);
+            });
+    }
+
+    downloadCsv(evt) {
+
+        var myData = this.tblData;
+
+        const fields = ['name', 'count'];
+        const opts = { fields };
+
+        const csv = parse(myData, opts);
+
+        var hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'people.csv';
+        hiddenElement.click();
     }
 }
